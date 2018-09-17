0c2f564bc7acaba0751cfbf35886ed01e5c65106
Alex Soto <asotobu@gmail.com>
2018-09-17 12:18:08 +0200

    Initial commit

---
+++ src/main/java/org/lordofthejars/games/game/GameService.java
@@ -0,0 +1,15 @@
+package org.lordofthejars.games.game;
+
+import java.util.Optional;
+import org.lordofthejars.games.reviews.api.ReviewService;
+
+public class GameService {
+
+    @Override
+    public Optional<Game> findGameById(long gameId) {
+
+
+    }
+
+
+}
